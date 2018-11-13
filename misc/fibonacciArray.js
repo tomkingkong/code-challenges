abe0de4682441f7a47bb7ac1fb69e9e147efbf72
Tom King <design.tomking@gmail.com>
2018-11-12 17:05:07 -0700

    Refactor fibarray to sort of recursive

--- misc/fibonacciArray.js
+++ misc/fibonacciArray.js
@@ -1,16 +1,28 @@
 // return the fibonacci sequence up to a given number (n)
 
-const fibArray = (n) => {
-  let seq = [0, 1];
+// const fibArray = (n) => {
+//   let seq = [0, 1];
 
-  for (let i=0; i<Infinity; i++) {
-    let a = seq[seq.length-2];
-    let b = seq[seq.length-1];
-    seq.push(a+b);
-    if (seq.length >= n) {
-      return seq.slice(0, n+1);
-    }
-  }
+//   for (let i=0; i<Infinity; i++) {
+//     let a = seq[seq.length-2];
+//     let b = seq[seq.length-1];
+
+//     seq.push(a+b);
+
+//     if (seq.length >= n) {
+//       return seq.slice(0, n+1);
+//     }
+//   }
+// }
+
+const fibArray = (seq, n) => {
+  if (seq.length >= n) {
+    return seq.slice(0, n+1);
+  } 
+  let a = seq[seq.length-2];
+  let b = seq[seq.length-1];
+  seq = [...seq, a+b];
+  return fibArray(seq, n);
 }
 
-console.log(fibArray(10));
+console.log(fibArray([0, 1], 10));
